cf48d53d0af87aa7088d74b4c0f56be9d37ca93d
itsh1ghn00n <88812317+itsh1ghn00n@users.noreply.github.com>
2026-04-17 23:41:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -20,41 +20,24 @@ app.use(passport.initialize());
 const router = express.Router();
 
 const crypto = require("crypto");
-const rp = require('request-promise');
 
 const GA_TRACKING_ID = process.env.GA_KEY;
 
 function trackDimension(category, action, label, value, dimension, metric) {
+  const params = new URLSearchParams({
+    v: '1',
+    tid: process.env.GA_KEY,
+    cid: crypto.randomBytes(16).toString("hex"),
+    t: 'event',
+    ec: category,
+    ea: action,
+    el: label,
+    ev: value,
+    cd1: dimension,
+    cm1: metric
+  });
 
-    var options = { method: 'GET',
-        url: 'https://www.google-analytics.com/collect',
-        qs:
-            {   // API Version.
-                v: '1',
-                // Tracking ID / Property ID.
-                tid: GA_TRACKING_ID,
-                // Random Client Identifier. Ideally, this should be a UUID that
-                // is associated with particular user, device, or browser instance.
-                cid: crypto.randomBytes(16).toString("hex"),
-                // Event hit type.
-                t: 'event',
-                // Event category.
-                ec: category,
-                // Event action.
-                ea: action,
-                // Event label.
-                el: label,
-                // Event value.
-                ev: value,
-                // Custom Dimension
-                cd1: dimension,
-                // Custom Metric
-                cm1: metric
-            },
-        headers:
-            {  'Cache-Control': 'no-cache' } };
-
-    return rp(options);
+  return fetch(`https://www.google-analytics.com/collect?${params.toString()}`);
 }
 
 router.post('/signup', async (req, res) => { // Use async/await
